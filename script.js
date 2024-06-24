88ce843f4b04ae8bb476e2f6f6fd52e36332646a
BrightTheBackpack <78445409+BrightTheBackpack@users.noreply.github.com>
2024-06-24 19:02:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var w = window.innerWidth;
+var h = window.innerHeight;
+var half = w/4
+console.log(half)
+addEventListener("resize", (event) => {});
+
+// onresize = (event) => {
+//   var w = window.innerWidth;
+//   var h = window.innerHeight;
+//   var half = w/3 - w/4
+//   document.getElementById("headertxt").style.fontSize = half-200 + "px";};
+// document.getElementById("headertxt").style.fontSize = half-200 + "px";
+function darkmode(){
+  console.log('test')
+  
+}
